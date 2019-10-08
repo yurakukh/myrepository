aabfbdcf0929807591374ab5d4ab2207ea87a211
Yura Kukharenko <kukharenko.yura@gmail.com>
2019-10-08 20:04:03 +0300

    lab6 added

--- lab3/lab3/Program.cs
+++ lab3/lab3/Program.cs
@@ -54,48 +54,4 @@ namespace lab3
     }
 }
 
-//Additional task
-//17.Дані координати (як цілі від 1 до 8) двох різних полів шахівниці. 
-//   Якщо тура за один хід може перейти з одного поля на інше, вивести логічне значення True, інакше вивести значення False. 
 
-//namespace lab3
-//{
-//    class Program
-//    {
-//        static void Main()
-//        {
-//            int x1, y1, x2, y2;
-//            Console.WriteLine("Coordinates can be in range [1;8]");
-//            Console.WriteLine("Enter first point");
-//            x1 = Input("x1");
-//            y1 = Input("y1");
-
-//            Console.WriteLine("Enter second point");
-//            x2 = Input("x2");
-//            y2 = Input("y2");
-
-
-//            Console.WriteLine("\n({0};{1}) --- ({2};{3})", x1, y1, x2, y2);
-//            if(x1 == x2 || y1 == y2)
-//            {
-//                Console.WriteLine("True");
-//            }
-//            else
-//            {
-//                Console.WriteLine("False");
-//            }
-//        }
-
-//        static int Input(string coordinate)
-//        {
-//            int x;
-//            do
-//            {
-//                Console.Write("{0} = ", coordinate);
-//                x = Convert.ToInt32(Console.ReadLine());
-//            } while (x < 1 || x > 8);
-
-//            return x;
-//        }
-//    }
-//}
---
+++ lab6/lab6/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lab6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            Console.Write("Enter a number: ");
+
+            //checking if number is an integer
+            while(!int.TryParse(Console.ReadLine(), out number))
+            {
+                Console.Write("ERROR! Enter a correct number: ");
+            }
+
+            //converting number to string
+            string num = number.ToString();
+
+            //creating a list
+            List<int> numList = new List<int>();
+
+            //filling the list 
+            for (int i = 0; i < num.Length; i++)
+            {
+                numList.Add(int.Parse(num[i].ToString()));
+            }
+
+            numList.Sort();
+
+            //counting each element in the list
+            int counter = 0;
+            for (int i = 0; i < numList.Count; i++)
+            {
+                for (int j = 0; j < numList.Count; j++)
+                {
+                    if (numList[i] == numList[j])
+                        counter++;
+                }
+                Console.WriteLine("Element {0} we have {1} time(s) here!", numList[i], counter);
+                i += counter - 1;
+                counter = 0;
+            }
+
+            //finding necessary number in the list
+            Console.Write("Enter a number you want to find: ");
+            int numToFind;
+            while (!int.TryParse(Console.ReadLine(), out numToFind))
+            {
+                Console.Write("ERROR! Enter a correct number: ");
+            }
+            for (int i = 0; i < numList.Count; i++)
+            {
+                if (numList[i] == numToFind)
+                {
+                    Console.WriteLine("Your number is here at {0} position.", i+1);
+                    return;
+                }
+            }
+            Console.WriteLine("No {0} in this list!", numToFind);
+
+        }
+    }
+}
