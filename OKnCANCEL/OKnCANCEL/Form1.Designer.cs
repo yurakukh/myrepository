d13466e7a07fbba48108ea7c3804369ad0e3e79b
Yura Kukharenko <kukharenko.yura@gmail.com>
2020-03-25 20:04:16 +0200

    second laba added

---
+++ OKnCANCEL/OKnCANCEL/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace OKnCANCEL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.XCoord = new System.Windows.Forms.Label();
+            this.YCoord = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.Color.Crimson;
+            this.button1.Location = new System.Drawing.Point(350, 208);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(230, 54);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "CANCEL";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.ForeColor = System.Drawing.Color.DarkOliveGreen;
+            this.button2.Location = new System.Drawing.Point(385, 321);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(159, 81);
+            this.button2.TabIndex = 0;
+            this.button2.TabStop = false;
+            this.button2.Text = "OK!";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // XCoord
+            // 
+            this.XCoord.AutoSize = true;
+            this.XCoord.Location = new System.Drawing.Point(845, 18);
+            this.XCoord.Name = "XCoord";
+            this.XCoord.Size = new System.Drawing.Size(25, 17);
+            this.XCoord.TabIndex = 1;
+            this.XCoord.Text = "X: ";
+            // 
+            // YCoord
+            // 
+            this.YCoord.AutoSize = true;
+            this.YCoord.Location = new System.Drawing.Point(845, 48);
+            this.YCoord.Name = "YCoord";
+            this.YCoord.Size = new System.Drawing.Size(21, 17);
+            this.YCoord.TabIndex = 2;
+            this.YCoord.Text = "Y:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(924, 542);
+            this.Controls.Add(this.YCoord);
+            this.Controls.Add(this.XCoord);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CLICK \'OK\' BUTTON!";
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label XCoord;
+        private System.Windows.Forms.Label YCoord;
+    }
+}
+
---
+++ OKnCANCEL/OKnCANCEL/Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OKnCANCEL
+{
+    public partial class Form1 : Form
+    {
+        private Timer timer;
+        private int count = 0;
+
+        //created this because I have 2 calls of SetTimer() func with 2 different blinking strings
+        private string blinkingMessage = "Click 'OK' button";
+        public Form1()
+        {
+
+            InitializeComponent();
+
+            SetTimer();
+
+            button2.GotFocus += new EventHandler(DeleteFocusFromButton);
+            
+        }
+
+        //disabling focus on 'OK' button
+        private void DeleteFocusFromButton(object sender , EventArgs e)
+        {
+            button1.Focus();
+        }
+        
+     
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("'OK' button was clicked");
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            this.XCoord.Text = "X: " + button2.Location.X.ToString();
+            this.YCoord.Text = "Y: " + button2.Location.Y.ToString();
+            int speed = 10;
+            int margin = 30;
+            int height = button2.Height;
+            int width = button2.Width;
+            int bX = button2.Location.X;
+            int bY = button2.Location.Y;
+
+            //for 4 sides
+            //left
+            if(e.X > bX - margin && e.X < bX && e.Y > bY && e.Y < bY + height)
+            {
+                button2.Location = new Point(button2.Location.X + speed, button2.Location.Y);
+            }
+            //right
+            if (e.X > bX + width && e.X < bX + width + margin && e.Y > bY && e.Y < bY + height)
+            {
+                button2.Location = new Point(button2.Location.X - speed, button2.Location.Y);
+            }
+            //top
+            if (e.X > bX && e.X < bX + width && e.Y > bY - margin && e.Y < bY)
+            {
+                button2.Location = new Point(button2.Location.X, button2.Location.Y + speed);
+            }
+            //bottom
+            if (e.X > bX && e.X < bX + width && e.Y > bY + height && e.Y < bY + height + margin)
+            {
+                button2.Location = new Point(button2.Location.X, button2.Location.Y - speed);
+            }
+            //left top 
+            if (e.X > bX - margin && e.X < bX && e.Y > bY - margin && e.Y < bY)
+            {
+                button2.Location = new Point(button2.Location.X + speed, button2.Location.Y + speed);
+                ReduceButtonSize(button2);
+            }
+            //right top
+            if (e.X > bX + width && e.X < bX + width + margin && e.Y > bY - margin && e.Y < bY)
+            {
+                button2.Location = new Point(button2.Location.X - speed, button2.Location.Y + speed);
+                ReduceButtonSize(button2);
+            }
+            //left bottom
+            if (e.X > bX - margin && e.X < bX && e.Y > bY + height && e.Y < bY + height + margin)
+            {
+                button2.Location = new Point(button2.Location.X + speed, button2.Location.Y - speed);
+                ReduceButtonSize(button2);
+            }
+            //right bottom
+            if (e.X > bX + width && e.X < bX + width + margin && e.Y > bY + height && e.Y < bY + height + margin)
+            {
+                button2.Location = new Point(button2.Location.X - speed, button2.Location.Y - speed);
+                ReduceButtonSize(button2);
+            }
+
+            //PUSHING THE BUTTON AWAY FROM THE EDGES
+            //left
+            if(bX < 1)
+            {
+                if (e.Y > bY + height/2)
+                    button2.Location = new Point(button2.Location.X + margin, button2.Location.Y - margin);
+                else
+                    button2.Location = new Point(button2.Location.X + margin, button2.Location.Y + margin);
+            }
+            //right
+            if(bX + width + 10 > this.Width)
+            {
+                if (e.Y > bY + height/2)
+                    button2.Location = new Point(button2.Location.X - margin, button2.Location.Y - margin);
+                else
+                    button2.Location = new Point(button2.Location.X - margin, button2.Location.Y + margin);
+            }
+            //bottom
+            if (bY > this.Height - height - 40)
+            {
+                if (e.X > bX + width/2)
+                    button2.Location = new Point(button2.Location.X - margin, button2.Location.Y - margin);
+                else
+                    button2.Location = new Point(button2.Location.X + margin, button2.Location.Y - margin);
+            }
+            //top
+            if (bY < 0)
+            {
+                if (e.X > bX + width/2)
+                    button2.Location = new Point(button2.Location.X - margin, button2.Location.Y + margin);
+                else
+                    button2.Location = new Point(button2.Location.X + margin, button2.Location.Y + margin);
+            }
+
+        }
+
+        private void ReduceButtonSize(Button b)
+        {
+            b.Width -= 1;
+            b.Height -= 1;
+            if(b.Width == 0 || b.Height == 0)
+            {
+                blinkingMessage = "'OK' button cannot be clicked :(";
+                SetTimer();
+            }
+        }
+
+       
+        private void Blinking(object sender, EventArgs e)
+        {
+           count++;
+           if(count % 2 == 0)
+            {
+                this.Text = blinkingMessage;
+                this.ForeColor = Color.Green;
+            }
+            else
+            {
+                this.Text = "";
+            }
+           if(count == 17)
+            {
+                this.Text = blinkingMessage;
+                timer.Stop();
+            }
+        }
+
+        private void SetTimer()
+        {
+            count = 0;
+            timer = new Timer();
+            timer.Interval = 500;
+            timer.Tick += new EventHandler(Blinking);
+            timer.Enabled = true;
+            timer.Start();
+        }
+
+
+        
+    }
+}
