f69d9877622d411eb7cd8fbce0cd318abdda42ec
Yura Kukharenko <kukharenko.yura@gmail.com>
2020-04-10 20:10:37 +0300

    laba 3 added

---
+++ Laboratory 3/Laboratory 3/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace Laboratory_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.dropDownList = new System.Windows.Forms.ComboBox();
+            this.inputField = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.AddItem = new System.Windows.Forms.Button();
+            this.DeleteItem = new System.Windows.Forms.Button();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(73, 29);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(858, 682);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.dropDownList);
+            this.tabPage1.Controls.Add(this.inputField);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.AddItem);
+            this.tabPage1.Controls.Add(this.DeleteItem);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(850, 653);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // dropDownList
+            // 
+            this.dropDownList.FormattingEnabled = true;
+            this.dropDownList.Location = new System.Drawing.Point(37, 207);
+            this.dropDownList.Name = "dropDownList";
+            this.dropDownList.Size = new System.Drawing.Size(121, 24);
+            this.dropDownList.TabIndex = 5;
+            // 
+            // inputField
+            // 
+            this.inputField.Location = new System.Drawing.Point(37, 78);
+            this.inputField.Name = "inputField";
+            this.inputField.Size = new System.Drawing.Size(160, 22);
+            this.inputField.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(48, 173);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(151, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Check Your items here";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(194, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Type an item you want to add";
+            // 
+            // AddItem
+            // 
+            this.AddItem.Location = new System.Drawing.Point(251, 78);
+            this.AddItem.Name = "AddItem";
+            this.AddItem.Size = new System.Drawing.Size(75, 23);
+            this.AddItem.TabIndex = 1;
+            this.AddItem.Text = "Add";
+            this.AddItem.UseVisualStyleBackColor = true;
+            this.AddItem.Click += new System.EventHandler(this.AddItem_Click);
+            // 
+            // DeleteItem
+            // 
+            this.DeleteItem.Location = new System.Drawing.Point(363, 78);
+            this.DeleteItem.Name = "DeleteItem";
+            this.DeleteItem.Size = new System.Drawing.Size(75, 23);
+            this.DeleteItem.TabIndex = 0;
+            this.DeleteItem.Text = "Delete";
+            this.DeleteItem.UseVisualStyleBackColor = true;
+            this.DeleteItem.Click += new System.EventHandler(this.DeleteItem_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(850, 653);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1079, 755);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.ComboBox dropDownList;
+        private System.Windows.Forms.TextBox inputField;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button AddItem;
+        private System.Windows.Forms.Button DeleteItem;
+    }
+}
+
---
+++ Laboratory 3/Laboratory 3/Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laboratory_3
+{
+    public partial class Form1 : Form
+    {
+
+        private Random rand = new Random();
+        private List<int> randomNumbers = new List<int>();
+        private List<Button> buttonList = new List<Button>();
+        ProgressBar progress = new ProgressBar();
+        Label finishGameLabel = new Label();
+
+
+        private int count = 1;
+
+        public Form1()
+        {
+            InitializeComponent();
+            CreateNumberField();
+        }
+
+        //tab 1
+        private void AddItem_Click(object sender, EventArgs e)
+        {
+            if(inputField.Text != "")
+            {
+                dropDownList.Items.Add(inputField.Text);
+                inputField.Clear();
+            }
+        }
+
+        private void DeleteItem_Click(object sender, EventArgs e)
+        {
+            if(dropDownList.Items.Count != 0)
+            {
+                dropDownList.Items.RemoveAt(dropDownList.Items.Count - 1);
+            }
+        }
+        //tab 1
+
+        //tab 2
+
+        private Button CreateButton(int xCoordinate, int yCoordinate)
+        {
+            Button button = new Button();
+            button.Size = new Size(50, 50);
+            button.Location = new Point(xCoordinate, yCoordinate);
+            return button;
+        }
+
+        private void CreateNumberField()
+        {
+            Button b;
+            int margin = 80;
+
+            int X = (this.tabControl1.TabPages[1].Width / 2) - (margin + 65);
+            int Y = 100;
+            int counter = 0;
+
+            for (int i = 0; i < 16; i++)
+            {
+                b = CreateButton(X, Y);
+
+                counter++;
+                X += margin;
+
+                if (counter == 4)
+                {
+                    Y += 60;
+                    X = (this.tabControl1.TabPages[1].Width / 2) - (margin + 65);
+                    counter = 0;
+                }
+
+                b.Click += new EventHandler(NumberClick);
+
+                this.tabControl1.TabPages[1].Controls.Add(b);
+                buttonList.Add(b);
+
+                
+            }
+            SetRandomNumbers(buttonList);
+
+            //creating progress bar and label
+            Y += 60;
+            X = (this.tabControl1.TabPages[1].Width / 2) - (margin + 65);
+            progress.Location = new Point(X, Y);
+            progress.Size = new Size(290, 30);
+            progress.Maximum = 112;
+            this.tabControl1.TabPages[1].Controls.Add(progress);
+
+            Y += 40;
+            finishGameLabel.Location = new Point(X, Y);
+            finishGameLabel.Size = new Size(290, 30);
+            finishGameLabel.Text = "Click in the correct sequence!";
+            finishGameLabel.Font = new Font("Roboto", 12f, FontStyle.Regular);
+            this.tabControl1.TabPages[1].Controls.Add(finishGameLabel);
+
+        }
+
+
+        private void NumberClick(object sender, EventArgs e)
+        {
+            if ((sender as Button).Text == count.ToString())
+            {
+                this.tabControl1.TabPages[1].Controls.Remove(sender as Button);
+                buttonList.Remove(sender as Button);
+                count++;
+                progress.Value += 7;
+                SetRandomNumbers(buttonList);
+                if(progress.Value == 112)
+                {
+                    finishGameLabel.Text = "Great job!!!";
+                }
+            }
+            else
+            {
+                this.tabControl1.TabPages[1].Controls.Clear();
+                buttonList.Clear();
+                count = 1;
+                progress.Value = 0;
+                CreateNumberField();
+            }
+        }
+
+
+        private void SetRandomNumbers(List<Button> list)
+        {
+            List<int> possible = new List<int>();
+
+
+            for (int i = count; i <= 16; i++)
+            {
+                possible.Add(i);             
+
+            }
+
+
+            int index;
+            
+            for (int i = 0; i < list.Count; i++)
+            {
+                index = rand.Next(0, possible.Count);
+                list[i].Text = " ";
+                list[i].Text = possible[index].ToString(); 
+                possible.RemoveAt(index); 
+                
+            }
+
+
+            
+        }
+
+     
+
+     
+    }
+}
