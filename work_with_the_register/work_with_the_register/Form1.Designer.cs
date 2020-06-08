766118c0007f1572a89fc5cdf2a3ef0f3cc0663a
Yura Kukharenko <kukharenko.yura@gmail.com>
2020-06-08 16:07:55 +0300

    work with saving parameters

---
+++ work_with_the_register/work_with_the_register/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace work_with_the_register
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(39, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(360, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "My prog can save it`s parameters. Try it!";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(44, 141);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(385, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(41, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Type smthng";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(94, 209);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(57, 21);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "click";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(258, 209);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(62, 21);
+            this.checkBox2.TabIndex = 4;
+            this.checkBox2.Text = "clack";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton4);
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(44, 264);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(385, 91);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "How to save?";
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(198, 35);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(68, 21);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.Text = "binary";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Checked = true;
+            this.radioButton2.Location = new System.Drawing.Point(114, 35);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(47, 21);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = ".txt";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(17, 35);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(61, 21);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.Text = "XML ";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(298, 35);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(76, 21);
+            this.radioButton4.TabIndex = 3;
+            this.radioButton4.Text = "registry";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(475, 380);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton4;
+    }
+}
+
---
+++ work_with_the_register/work_with_the_register/Form1.cs
@@ -0,0 +1,308 @@
+﻿using System;
+using System.Xml.Linq;
+using System.Drawing;
+using System.IO;
+
+using System.Windows.Forms;
+using System.Xml;
+using System.Runtime.InteropServices;
+using Microsoft.Win32;
+using System.Runtime.CompilerServices;
+
+namespace work_with_the_register
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (radioButton1.Checked) SaveToXML();
+            else if (radioButton2.Checked) SaveToTxt();
+            else if (radioButton3.Checked) SaveToBinary();
+            else SaveToRegistry();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //different ways to read params
+            //ReadFromXML();
+            //ReadFromTxt();
+            //ReadFromBinary();
+            ReadFromRegistry();
+        }
+
+
+
+        //functions to save and restore parameters
+        private void SaveToTxt()
+        {
+            string writePath = @".\params.txt";
+
+
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(writePath, false, System.Text.Encoding.Default))
+                {
+                    sw.WriteLine(this.Location.X);
+                    sw.WriteLine(this.Location.Y);
+                    sw.WriteLine(this.Width);
+                    sw.WriteLine(this.Height);
+                    sw.WriteLine(this.textBox1.Text);
+                    sw.WriteLine(this.checkBox1.Checked);
+                    sw.WriteLine(this.checkBox2.Checked);
+                    sw.WriteLine(this.radioButton1.Checked);
+                    sw.WriteLine(this.radioButton2.Checked);
+                    sw.WriteLine(this.radioButton3.Checked);
+                    sw.WriteLine(this.radioButton4.Checked);
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void ReadFromTxt()
+        {
+            string readPath = @".\params.txt";
+            var file = new FileInfo(readPath);
+            if (file.Exists && file.Length != 0)
+            {
+
+
+                try
+                {
+                    using (StreamReader sr = new StreamReader(readPath, System.Text.Encoding.Default))
+                    {
+                        this.Location = new Point(Convert.ToInt32(sr.ReadLine()), Convert.ToInt32(sr.ReadLine()));
+                        this.Width = Convert.ToInt32(sr.ReadLine());
+                        this.Height = Convert.ToInt32(sr.ReadLine());
+                        this.textBox1.Text = sr.ReadLine();
+                        this.checkBox1.Checked = Convert.ToBoolean(sr.ReadLine());
+                        this.checkBox2.Checked = Convert.ToBoolean(sr.ReadLine());
+
+                        this.radioButton1.Checked = Convert.ToBoolean(sr.ReadLine());
+                        this.radioButton2.Checked = Convert.ToBoolean(sr.ReadLine());
+                        this.radioButton3.Checked = Convert.ToBoolean(sr.ReadLine());
+                        this.radioButton4.Checked = Convert.ToBoolean(sr.ReadLine());
+
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+     
+        private void SaveToXML()
+        {
+            
+
+            try
+            {
+
+                var xmlDoc = new XDocument(new XDeclaration("1.0", "utf-8", "yes"),
+                    new XElement("Parameters",
+                        new XElement("X", this.Location.X),
+                        new XElement("Y", this.Location.Y),
+                        new XElement("Width", this.Width),
+                        new XElement("Height", this.Height),
+                        new XElement("Text", this.textBox1.Text),
+                        new XElement("checkBox_1", this.checkBox1.Checked),
+                        new XElement("checkBox_2", this.checkBox2.Checked),
+                        new XElement("rb_1", this.radioButton1.Checked),
+                        new XElement("rb_2", this.radioButton2.Checked),
+                        new XElement("rb_3", this.radioButton3.Checked),
+                        new XElement("rb_4", this.radioButton3.Checked)
+                    ));
+
+
+                //save it to the current directory
+                xmlDoc.Save(Path.Combine(Environment.CurrentDirectory, "xmlParams.xml"));
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void ReadFromXML()
+        {
+
+            string xmlPath = @".\xmlParams.xml";
+            var file = new FileInfo(xmlPath);
+            if (file.Exists && file.Length != 0)
+            {
+                XmlTextReader reader = new XmlTextReader(xmlPath);
+                reader.ReadToFollowing("Parameters");
+                reader.MoveToFirstAttribute();
+
+                //location
+                reader.ReadToFollowing("X");
+                Point p = new Point();
+                p.X = Convert.ToInt32(reader.ReadElementContentAsString());
+
+                reader.ReadToFollowing("Y");
+                p.Y = Convert.ToInt32(reader.ReadElementContentAsString());
+
+                this.Location = p;
+
+                //sizes
+                Size s = new Size();
+
+                reader.ReadToFollowing("Width");
+                s.Width = Convert.ToInt32(reader.ReadElementContentAsString());
+                reader.ReadToFollowing("Height");
+                s.Height = Convert.ToInt32(reader.ReadElementContentAsString());
+
+                this.Size = s;
+
+                //text
+                reader.ReadToFollowing("Text");
+                this.textBox1.Text = reader.ReadElementContentAsString();
+
+                //checkboxes
+                reader.ReadToFollowing("checkBox_1");
+                this.checkBox1.Checked = Convert.ToBoolean(reader.ReadElementContentAsString());
+                reader.ReadToFollowing("checkBox_2");
+                this.checkBox2.Checked = Convert.ToBoolean(reader.ReadElementContentAsString());
+
+                //radio
+                reader.ReadToFollowing("rb_1");
+                this.radioButton1.Checked = Convert.ToBoolean(reader.ReadElementContentAsString());
+                reader.ReadToFollowing("rb_2");
+                this.radioButton2.Checked = Convert.ToBoolean(reader.ReadElementContentAsString());
+                reader.ReadToFollowing("rb_3");
+                this.radioButton3.Checked = Convert.ToBoolean(reader.ReadElementContentAsString());
+                reader.ReadToFollowing("rb_4");
+                this.radioButton4.Checked = Convert.ToBoolean(reader.ReadElementContentAsString());
+                reader.Close();
+
+                file.Delete();
+            }
+        }
+
+
+        private void SaveToBinary()
+        {
+            string writePath = @".\params.dat";
+
+            try
+            {
+                using (BinaryWriter bw = new BinaryWriter(File.Open(writePath, FileMode.OpenOrCreate)))
+                {
+                    bw.Write(this.Location.X);
+                    bw.Write(this.Location.Y);
+                    bw.Write(this.Width);
+                    bw.Write(this.Height);
+                    bw.Write(this.textBox1.Text);
+                    bw.Write(this.checkBox1.Checked);
+                    bw.Write(this.checkBox2.Checked);
+                    bw.Write(this.radioButton1.Checked);
+                    bw.Write(this.radioButton2.Checked);
+                    bw.Write(this.radioButton3.Checked);
+                    bw.Write(this.radioButton4.Checked);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void ReadFromBinary()
+        {
+            string readPath = @".\params.dat";
+            FileInfo file = new FileInfo(readPath);
+            if (file.Exists && file.Length != 0)
+            {
+                try
+                {
+                    using (BinaryReader br = new BinaryReader(File.Open(readPath, FileMode.Open)))
+                    {
+                        while (br.PeekChar() > -1)
+                        {
+                            Point p = new Point(br.ReadInt32(), br.ReadInt32());
+                            this.Width = br.ReadInt32();
+                            this.Height = br.ReadInt32();
+                            this.textBox1.Text = br.ReadString();
+                            this.checkBox1.Checked = br.ReadBoolean();
+                            this.checkBox2.Checked = br.ReadBoolean();
+                            this.radioButton1.Checked = br.ReadBoolean();
+                            this.radioButton2.Checked = br.ReadBoolean();
+                            this.radioButton3.Checked = br.ReadBoolean();
+                            this.radioButton4.Checked = br.ReadBoolean();
+                        }
+
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private void SaveToRegistry()
+        {
+            RegistryKey currentUser = Registry.CurrentUser;
+            RegistryKey paramsKey = currentUser.CreateSubKey("Parameters");
+
+            paramsKey.SetValue("X", this.Location.X);
+            paramsKey.SetValue("Y", this.Location.Y);
+            paramsKey.SetValue("Width", this.Width);
+            paramsKey.SetValue("Height", this.Height);
+            paramsKey.SetValue("Text", this.textBox1.Text);
+            paramsKey.SetValue("checkBox_1", this.checkBox1.Checked);
+            paramsKey.SetValue("checkBox_2", this.checkBox2.Checked);
+            paramsKey.SetValue("rb_1", this.radioButton1.Checked);
+            paramsKey.SetValue("rb_2", this.radioButton2.Checked);
+            paramsKey.SetValue("rb_3", this.radioButton3.Checked);
+            paramsKey.SetValue("rb_4", this.radioButton4.Checked);
+            
+            paramsKey.Close();
+        }
+
+        private void ReadFromRegistry()
+        {
+            try
+            {
+                RegistryKey paramsKey = Registry.CurrentUser.CreateSubKey("Parameters");
+                if (paramsKey.ValueCount != 0)
+                {
+                    Point p = new Point();
+                    p.X = Convert.ToInt32(paramsKey.GetValue("X"));
+                    p.Y = Convert.ToInt32(paramsKey.GetValue("Y"));
+                    this.Location = p;
+                    this.Width = Convert.ToInt32(paramsKey.GetValue("Width"));
+                    this.Height = Convert.ToInt32(paramsKey.GetValue("Height"));
+                    this.Text = paramsKey.GetValue("Text").ToString();
+                    this.checkBox1.Checked = Convert.ToBoolean(paramsKey.GetValue("checkBox_1"));
+                    this.checkBox2.Checked = Convert.ToBoolean(paramsKey.GetValue("checkBox_2"));
+                    this.radioButton1.Checked = Convert.ToBoolean(paramsKey.GetValue("rb_1"));
+                    this.radioButton2.Checked = Convert.ToBoolean(paramsKey.GetValue("rb_2"));
+                    this.radioButton3.Checked = Convert.ToBoolean(paramsKey.GetValue("rb_3"));
+                    this.radioButton4.Checked = Convert.ToBoolean(paramsKey.GetValue("rb_4"));
+
+                    //close
+                    paramsKey.Close();
+                    Registry.CurrentUser.DeleteSubKey("Parameters");
+                }
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+    }
+}
